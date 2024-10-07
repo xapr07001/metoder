878ecbd23e8fe56ef3387f1b2f2df7c5289e2a49
xapr07001 <xapr07001@utb.vaxjo.se>
2024-10-07 10:30:52 +0200

    metoder

--- Program.cs
+++ Program.cs
@@ -5,14 +5,13 @@ double CirkelOmkrets(double r){
     return((r+r)*3.1415);
 }
 
-double radie=2.4;
-Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
-Console.WriteLine("Cirkelns area: "+CirkelArea(radie));
-
-
 double Average(double a, double b, double c){
     return((a+b+c)/3);
 }
+
+double radie=2.4;
+Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
+Console.WriteLine("Cirkelns area: "+CirkelArea(radie));
 
 double a = 5.2, b = 3.4, c = 1.5;
 Console.WriteLine("medelvärdet är " + Average(a,b,c));
